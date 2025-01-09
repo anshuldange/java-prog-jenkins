e016fed77a632a7a509a08f3ccee808bc550a586
anshuldange <165261215+anshuldange@users.noreply.github.com>
2025-01-09 12:31:38 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is learning project for jenkins");
+        System.out.println("Hello form- Main class");
+        System.out.println(LocalDate.now());
+
+    }
+}
